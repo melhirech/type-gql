3649f3e7f4d033b1e9349e6d4375d48390165186
Mohamed Elhirech <elhirech.95@gmail.com>
2019-03-29 18:49:31 +0000

    add: logout resolver

---
+++ src/modules/user/Logout.ts
@@ -0,0 +1,19 @@
+import { MyContext } from './../../../types/MyContext';
+import { Resolver, Mutation, Ctx } from "type-graphql";
+
+@Resolver()
+export class LogoutResolver {
+  @Mutation(() => Boolean)
+  async logout(
+      @Ctx() ctx: MyContext
+  ): Promise<boolean> {
+      return new Promise((resolve, reject) => {
+          ctx.req.session!.destroy(err => {
+              if(err) return reject(false)
+
+              ctx.res.clearCookie('qid')
+              return resolve(true)
+          })
+      })
+  }
+}
